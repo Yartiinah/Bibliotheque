dce93a41386b88b28106abebf7bfd2801b649a9f
Yartiinah <ramiandrisoatitie@gmail.com>
2025-07-15 08:24:38 +0200

    update-testFindByUsername

--- src/test/java/test/TestFindByUsername.java
+++ src/test/java/test/TestFindByUsername.java
@@ -1,27 +1,23 @@
 package test;
 
 import model.Utilisateur;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import repository.UtilisateurRepository;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-@SpringBootTest
 public class TestFindByUsername {
-
-    @Autowired
-    private UtilisateurRepository utilisateurRepository;
-
     @Test
     public void testFindByUsername() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:WEB-INF/app-context.xml");
+        UtilisateurRepository utilisateurRepository = context.getBean(UtilisateurRepository.class);
         Utilisateur user = utilisateurRepository.findByUsername("test").orElse(null);
         if (user != null) {
             System.out.println("Trouvé : " + user.getUsername() + ", mot de passe : " + user.getPassword());
         } else {
             System.out.println("Non trouvé");
         }
-        assertNotNull(user);
+        Assert.assertNotNull(user);
     }
 }
